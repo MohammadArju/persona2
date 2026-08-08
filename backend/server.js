2c461489263977b814b95108fddf2e9253e3c5fa
MohammadArju <mudassiransari8322@gmail.com>
2026-08-09 01:09:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -136,6 +136,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-// Server start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
